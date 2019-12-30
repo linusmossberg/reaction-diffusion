e06393480e8834a60887bfe957c9e1757811c350
Linus Mossberg <linmo400@student.liu.se>
2019-12-30 15:20:33 +0100

    Add anisotropic diffusion and improve color map

--- js/reaction-diffusion-fragment.js
+++ js/reaction-diffusion-fragment.js
@@ -1,39 +1,84 @@
 var reaction_diffusion_fragment = `
 
 const float scale = 2.0;
-const float Da = 1.0 * scale;
-const float Db = 0.5 * scale;
-const float Dt = 1.0 / scale;
-const float F  = 0.0545;
-const float K  = 0.062;
-//const float F = 0.0367;
-//const float K = 0.0649;
+const vec2 D = vec2(1, 0.5) * scale;
+const float Dt = 1.0/scale;
 
+// const float F  = 0.072;
+// const float K  = 0.062;
+
+// const float F  = 0.0367;
+// const float K  = 0.0649;
+
+const float F  = 0.045;
+const float K  = 0.06;
+
+// Sampling function with offset around current texel
 vec2 s(float x_offset, float y_offset)
 {
   return texture2D(reaction_diffusion, (gl_FragCoord.xy + vec2(x_offset, y_offset)) / resolution).xy;
 }
 
+// Finite differences laplace operator
+vec2 laplace(vec2 center)
+{
+  vec2
+  v00 = s(-1.0, 1.0), v10 = s(0.0, 1.0), v20 = s(1.0, 1.0),
+  v01 = s(-1.0, 0.0),                    v21 = s(1.0, 0.0),
+  v02 = s(-1.0,-1.0), v12 = s(0.0,-1.0), v22 = s(1.0,-1.0);
+
+  return 0.05 * (v00 + v20 + v02 + v22) + 0.2 * (v10 + v01 + v21 + v12) - center;
+}
+
+float p2(float v)
+{
+  return v * v;
+}
+
+vec2 anisotropicDiffusion(vec2 dir, float a, vec2 center)
+{
+  vec2
+  v00 = s(-1.0, 1.0), v10 = s(0.0, 1.0), v20 = s(1.0, 1.0),
+  v01 = s(-1.0, 0.0),                    v21 = s(1.0, 0.0),
+  v02 = s(-1.0,-1.0), v12 = s(0.0,-1.0), v22 = s(1.0,-1.0);
+
+  float a1 = clamp(a, 0.0, 1.0);
+  float a2 = 1.0 - a1;
+
+  float cos_t = dir.x;
+  float sin_t = dir.y;
+  float cos2_t = p2(cos_t);
+  float sin2_t = p2(sin_t);
+
+  float d = ((a2 - a1) * p2(cos_t * sin_t)) / 2.0;
+  float h = (a1 * cos2_t + a2 * sin2_t) / 2.0 - 0.05;
+  float v = (a2 * cos2_t + a1 * sin2_t) / 2.0 - 0.05;
+
+  vec2 result = (-d+0.05) * (v00 + v22) +
+                ( d+0.05) * (v20 + v02) +
+                h * (v01 + v21) +
+                v * (v10 + v12)
+                - center;
+
+  return result;
+}
+
 void main() 
 {
-  vec2 v00 = s(-1., 1.); vec2 v10 = s(0., 1.); vec2 v20 = s(1., 1.);
-  vec2 v01 = s(-1., 0.); vec2 v11 = s(0., 0.); vec2 v21 = s(1., 0.);
-  vec2 v02 = s(-1.,-1.); vec2 v12 = s(0.,-1.); vec2 v22 = s(1.,-1.);
+  // Old substance concentrations
+  vec2 old = s(0.0, 0.0);
 
-  float A = v11.x;
-  float B = v11.y;
+  // Convert some of substance 0 to substance 1
+  vec2 reaction = vec2(-1.0, 1.0) * old[0] * old[1] * old[1];
 
-  vec2 laplace = 0.05 * (v00 + v20 + v02 + v22) + 0.2 * (v10 + v01 + v21 + v12) - v11;
+  // Add some substance 0 and remove some substance 1
+  vec2 dissipation = vec2(F * (1.0 - old[0]), -old[1] * (K + F));      
 
-  float laplace_A = laplace.x;
-  float laplace_B = laplace.y;
+  // Diffuse substances at different rates specified by D
+  vec2 diffusion = laplace(old) * D;
 
-  float ABB = A * B * B;
-
-  float A_new = A + (Da * laplace_A - ABB + F * (1. - A))*Dt;
-  float B_new = B + (Db * laplace_B + ABB - B * (K  + F))*Dt;
-
-  gl_FragColor = vec4(A_new, B_new, 0.5, 1.0);
+  // New substance concentrations
+  gl_FragColor.xy = old + (reaction + dissipation + diffusion) * Dt;
 }
 
 `;
--- js/render-shaders.js
+++ js/render-shaders.js
@@ -4,7 +4,7 @@ var render_vertex = `
 
   void main() 
   {
-    texcoord = position.xy + vec2(0.5,0.5);
+    texcoord = uv;
 
     vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
     gl_Position = projectionMatrix * modelViewPosition;
@@ -15,38 +15,13 @@ var render_vertex = `
 var render_fragment = `
 
   uniform sampler2D reaction_diffusion;
+  uniform sampler2D color_map;
   varying vec2 texcoord;
-
-  const vec3 color0 = vec3(215., 25., 28.) / 255.;
-  const vec3 color1 = vec3(253.,174., 97.) / 255.;
-  const vec3 color2 = vec3(255.,255.,191.) / 255.;
-  const vec3 color3 = vec3(171.,217.,233.) / 255.;
-  const vec3 color4 = vec3( 44.,123.,182.) / 255.;
-
-  const vec3 r = vec3(.3, .4, .5);
-
-  vec3 transfer(float v)
-  { 
-    if(v < r[0])
-    {
-      return mix(color0, color1, v / r[0]);
-    }
-    if(v < r[1])
-    {
-      return mix(color1, color2, (v - r[0]) / (r[1] - r[0]));
-    }
-    if(v < r[2])
-    {
-      return mix(color2, color3, (v - r[1]) / (r[2] - r[1]));
-    }
-    return mix(color3, color4, (v - r[2]) / (1. - r[2]));
-  }
 
   void main() 
   {
-    vec2 components = texture2D(reaction_diffusion, texcoord).xy;
-    float v = components.g;
-    gl_FragColor = vec4(transfer(v), 1.0);
+    float v = texture2D(reaction_diffusion, texcoord).g * 2.0;
+    gl_FragColor = texture2D(color_map, vec2(v, 0.));
   }
 
 `;
